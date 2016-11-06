89f36cb1faceb38b5f61dbe551744a811b02d586
Adam Alton <adamalton@gmail.com>
2016-11-06 16:15:52 +0000

    Remove commented out JS code (declarativeContent).

--- https-finder/https-finder.js
+++ https-finder/https-finder.js
@@ -364,28 +364,3 @@ chrome.webNavigation.onCommitted.addListener(
 
 chrome.notifications.onButtonClicked.addListener(notificationButtonClicked);
 chrome.notifications.onClosed.addListener(notificationClosed);
-
-
-
-/*
-// When the extension is installed or upgraded ...
-chrome.runtime.onInstalled.addListener(function() {
-  // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-	// With a new rule ...
-	chrome.declarativeContent.onPageChanged.addRules([
-	  {
-		// That fires when a page's URL contains a 'g' ...
-		conditions: [
-		  new chrome.declarativeContent.PageStateMatcher({
-			pageUrl: { urlContains: 'g' },
-		  })
-		],
-		// And shows the extension's page action.
-		actions: [ new chrome.declarativeContent.ShowPageAction() ]
-	  }
-	]);
-  });
-});
-
-*/
